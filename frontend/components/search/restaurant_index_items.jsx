3716d097ed1f98fe795f575c08630d14f3a662c7
Jess Bon <j_bon@comcast.net>
2020-05-26 17:06:38 -0700

    QOL adjustment for index item links

--- frontend/components/search/restaurant_index_items.jsx
+++ frontend/components/search/restaurant_index_items.jsx
@@ -33,7 +33,7 @@ class RestaurantIndexItem extends React.Component {
         }
 
         return (
-            <div className="restaurant-ii-container">
+            <div className="restaurant-ii-container" onClick={this.handleClick}>
                 <div className="restaurant-ii-photo" onClick={this.handleClick} >
                     <img src={this.props.photos[0]} alt=""/>
                 </div>
